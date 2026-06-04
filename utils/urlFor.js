f5dc2d182e7e6a3bbc8e45bd160ee5f92532ccca
Sourav <sourav@jellylogic.in>
2026-06-04 14:58:22 +0530

    refactor: Simplify project section mapping and enhance URL builder functionality
    - Updated the project section mapping in `ProjectPage` to remove unnecessary index parameter.
    - Introduced a new `cloudinaryUrlBuilder` function in `urlFor.js` to handle Cloudinary asset URLs, improving image handling logic.

--- utils/urlFor.js
+++ utils/urlFor.js
@@ -1,8 +1,36 @@
 import sanityClient from '../client'
 import imageUrlBuilder from '@sanity/image-url'
 
+// Sanity URL Builder
 const builder = imageUrlBuilder(sanityClient)
-  
+
+/**
+ * Cloudinary URL Builder
+ * @param {Object} source - The source object
+ * @returns {Object} - The chain object
+ */
+function cloudinaryUrlBuilder(source) {
+  const url = source.secure_url || source.url
+  const chain = () => chain
+  chain.url = () => url
+  chain.width = chain
+  chain.height = chain
+  chain.quality = chain
+  chain.fit = chain
+  chain.format = chain
+  chain.auto = chain
+  chain.crop = chain
+  return chain
+}
+
+/**
+ * URL Builder
+ * @param {Object} source - The source object
+ * @returns {Object} - The chain object
+ */
 export default function urlFor(source) {
-    return builder.image(source)
+  if (source?._type === 'cloudinary.asset') {
+    return cloudinaryUrlBuilder(source)
+  }
+  return builder.image(source)
 }
